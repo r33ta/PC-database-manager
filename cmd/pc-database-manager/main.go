4730c0e52892c1e717a897a8dbf5cf2170233c50
r33ta <sandytwix10@gmail.com>
2024-05-15 18:16:14 +0300

    temporary testing added

--- cmd/pc-database-manager/main.go
+++ cmd/pc-database-manager/main.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/r33ta/pc-database-manager/internal/config"
 	"github.com/r33ta/pc-database-manager/internal/lib/logger/sl"
+	"github.com/r33ta/pc-database-manager/internal/models/memory"
+	"github.com/r33ta/pc-database-manager/internal/models/pc_storage"
 	"github.com/r33ta/pc-database-manager/internal/storage/sqlite"
 )
 
@@ -29,7 +31,27 @@ func main() {
 		os.Exit(1)
 	}
 
-	_ = storage
+	// testing
+	memoryID, err := storage.SaveMemory("Kingston RAM", memory.DDR4, 32)
+	if err != nil {
+		log.Error("failed to save memory", sl.Err(err))
+	}
+	cpuID, err := storage.SaveCpu("Intel Core i7-9700K", 8, 8, 3600)
+	if err != nil {
+		log.Error("failed to save cpu", sl.Err(err))
+	}
+	gpuID, err := storage.SaveGpu("Nvidia RTX 2080", "ASUS", 8, 1800)
+	if err != nil {
+		log.Error("failed to save gpu", sl.Err(err))
+	}
+	storageID, err := storage.SaveStorage("Kingston SSD 1TB", 1000, pc_storage.SSD)
+	if err != nil {
+		log.Error("failed to save storage", sl.Err(err))
+	}
+	_, err = storage.SavePC("Some PC", memoryID, cpuID, gpuID, storageID)
+	if err != nil {
+		log.Error("failed to save pc", sl.Err(err))
+	}
 
 	// TODO: init router: chi, "chi render"
 
