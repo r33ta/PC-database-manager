4dec4179d0d45e857007ab285ee79bb2c8249908
r33ta <sandytwix10@gmail.com>
2024-05-15 18:15:50 +0300

    refactoring

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -3,9 +3,14 @@ package storage
 import "errors"
 
 var (
-	ErrPCNotFound      = errors.New("pc not found")
-	ErrMemoryNotFound  = errors.New("memory not found")
-	ErrCpuNotFound     = errors.New("cpu not found")
-	ErrGpuNotFound     = errors.New("gpu not found")
-	ErrStorageNotFound = errors.New("storage not found")
+	ErrPCNotFound           = errors.New("pc not found")
+	ErrPCAlreadyExists      = errors.New("pc already exists")
+	ErrMemoryNotFound       = errors.New("memory not found")
+	ErrCpuAlreadyExists     = errors.New("cpu already exists")
+	ErrCpuNotFound          = errors.New("cpu not found")
+	ErrGpuAlreadyExists     = errors.New("gpu already exists")
+	ErrGpuNotFound          = errors.New("gpu not found")
+	ErrStorageAlreadyExists = errors.New("storage already exists")
+	ErrStorageNotFound      = errors.New("storage not found")
+	ErrMemoryAlreadyExists  = errors.New("memory already exists")
 )
