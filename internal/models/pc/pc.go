ab147c283b7581ac582d23166cb800cc34bb899d
r33ta <sandytwix10@gmail.com>
2024-05-16 20:52:21 +0300

    refactoring

--- internal/models/pc/pc.go
+++ internal/models/pc/pc.go
@@ -1,10 +1,10 @@
 package pc
 
 type PC struct {
-	ID        int
-	Name      string
-	MemoryID  int
-	CPUID     int
-	GPUID     int
-	StorageID int
+	ID        int64  `json:"id"`
+	Name      string `json:"name"`
+	MemoryID  int64  `json:"memory_id"`
+	CPUID     int64  `json:"cpu_id"`
+	GPUID     int64  `json:"gpu_id"`
+	StorageID int64  `json:"storage_id"`
 }
