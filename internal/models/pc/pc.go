9ae90ec1448d5094fa414038e0ed15312eba0794
r33ta <sandytwix10@gmail.com>
2024-05-18 19:16:28 +0300

    refactoring

--- internal/models/pc/pc.go
+++ internal/models/pc/pc.go
@@ -1,10 +1,10 @@
 package pc
 
 type PC struct {
-	ID        int64
-	Name      string
-	RAMID     int64
-	CPUID     int64
-	GPUID     int64
-	StorageID int64
+	ID       int64
+	Name     string
+	RAMID    int64
+	CPUID    int64
+	GPUID    int64
+	memoryID int64
 }
