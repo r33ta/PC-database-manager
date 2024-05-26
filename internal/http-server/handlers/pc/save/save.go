81e1717eae89d6381327d04c144cc435d8f595c2
r33ta <sandytwix10@gmail.com>
2024-05-26 09:53:38 +0300

    save handlers added

--- internal/http-server/handlers/pc/save/save.go
+++ internal/http-server/handlers/pc/save/save.go
@@ -11,7 +11,12 @@ import (
 	"github.com/r33ta/pc-database-manager/internal/lib/logger/sl"
 )
 
-type Request struct {
+type Response struct {
+	resp.Response
+	Name string `json:"name"`
+}
+
+type RequestPC struct {
 	Name     string `json:"name"`
 	RAMID    int64  `json:"ram_id" validate:"required"`
 	CPUID    int64  `json:"cpu_id" validate:"required"`
@@ -19,25 +24,190 @@ type Request struct {
 	MemoryID int64  `json:"memory_id" validate:"required"`
 }
 
-type Response struct {
-	resp.Response
-	Name string `json:"name"`
+type RequestRAM struct {
+	Name        string `json:"name"`
+	Memory_type string `json:"memory_type" validate:"required"`
+	Capacity    int64  `json:"capacity" validate:"required"`
+}
+
+type RequestCPU struct {
+	Name      string `json:"name"`
+	Cores     int64  `json:"cores" validate:"required"`
+	Threads   int64  `json:"threads" validate:"required"`
+	Frequency int64  `json:"frequency" validate:"required"`
+}
+
+type RequestGPU struct {
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer" validate:"required"`
+	Memory       int64  `json:"memory" validate:"required"`
+	Frequency    int64  `json:"frequency" validate:"required"`
+}
+
+type RequestMemory struct {
+	Name        string `json:"name"`
+	Capacity    int64  `json:"capacity" validate:"required"`
+	StorageType string `json:"storage_type" validate:"required"`
 }
 
 type PCSaver interface {
 	SavePC(name string, ramID, cpuID, gpuID, memoryID int64) (int64, error)
 }
 
-func New(log *slog.Logger, pcSaver PCSaver) http.HandlerFunc {
+type RAMSaver interface {
+	SaveRAM(name, memory_type string, capacity int64) (int64, error)
+}
+
+type CPUSaver interface {
+	SaveCPU(name string, cores, threads, frequency int64) (int64, error)
+}
+
+type GPUSaver interface {
+	SaveGPU(name, manufacturer string, memory, frequency int64) (int64, error)
+}
+
+type MemorySaver interface {
+	SaveMemory(name string, capacity int64, storage_type string) (int64, error)
+}
+
+func NewPC(log *slog.Logger, pcSaver PCSaver) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		const op = "handlers.save.New"
+		const op = "handlers.save.NewPC"
 
 		log = log.With(
 			slog.String("op", op),
 			slog.String("request_id", middleware.GetReqID(r.Context())),
 		)
 
-		var req Request
+		var req RequestPC
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request body"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+	}
+}
+
+func NewRAM(log *slog.Logger, ramSaver RAMSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.save.NewRAM"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req RequestRAM
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request body"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+	}
+}
+
+func NewCPU(log *slog.Logger, cpuSaver CPUSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.save.NewCPU"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req RequestCPU
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request body"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+	}
+}
+
+func NewGPU(log *slog.Logger, gpuSaver GPUSaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.save.NewGPU"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req RequestGPU
+
+		err := render.DecodeJSON(r.Body, &req)
+		if err != nil {
+			log.Error("failed to decode request body", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("failed to decode request body"))
+
+			return
+		}
+
+		log.Info("request body decoded", slog.Any("request", req))
+
+		if err := validator.New().Struct(req); err != nil {
+			log.Error("invalid request", sl.Err(err))
+
+			render.JSON(w, r, resp.Error("invalid request"))
+
+			return
+		}
+	}
+}
+
+func NewMemory(log *slog.Logger, memorySaver MemorySaver) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		const op = "handlers.save.NewMemory"
+
+		log = log.With(
+			slog.String("op", op),
+			slog.String("request_id", middleware.GetReqID(r.Context())),
+		)
+
+		var req RequestMemory
 
 		err := render.DecodeJSON(r.Body, &req)
 		if err != nil {
